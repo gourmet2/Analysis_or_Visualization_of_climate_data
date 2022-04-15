46f8537806029c2c5dcd106a8dfde35839728787
gourmet <57789025+gourmet2@users.noreply.github.com>
2022-04-15 20:41:02 +0800

    Update Fig_1_Global_pattern_in_grid_format.m

--- Fig_1_Global_pattern_in_grid_format.m
+++ Fig_1_Global_pattern_in_grid_format.m
@@ -1,24 +1,26 @@
-%
+% First, we need to have a matrix, for example, the gridded global data. Here we set the pixel without value as NAN.
+% matrix_plot is the matrix you'd like to visulize
 
 clc;clear
 
-ax = worldmap([-90 90],[-180 180]); %显示的地图范围为-180-180
+R = makerefmat('RasterSize', size(matrix_plot),'Latlim', [-90 90], 'Lonlim', [-180 180]); % The latlim & lonlim here depends on the geographic area of your matrix.
 
-setm(ax,'mapprojection','robinson'); %robinson生成圆形的图，Mercator则显示很长的地图,miller生成短地图
-setm(ax,'ParallelLabel','off');
-setm(ax,'MeridianLabel','off');
-set(ax,'Linewidth',0.5,'fontsize',12);
+ax = worldmap([-90 90],[-180 180]); % choose the display area of the map. This should have the area as your matrix   
 
-R = makerefmat('RasterSize', size(matrix_plot),'Latlim', [-90 90], 'Lonlim', [-180 180]);%lat2 %这里的Lonlim和worldmap中的精度范围保持一致
+setm(ax,'mapprojection','robinson'); % choose the projection of the map. Robinson，Mercator, and Miller are three widely-used projection
+setm(ax,'ParallelLabel','off'); % show no latitude information
+setm(ax,'MeridianLabel','off'); % show no longitute information
+set(ax,'Linewidth',0.5,'fontsize',12); % set the fontsize and linewidth. But changes is not obvious when adjusting the linewidth
 
-h=geoshow(flip(matrix_plot),R,'DisplayType','texturemap','facealpha','texture');
+h = geoshow(flip(matrix_plot), R, 'DisplayType','texturemap','facealpha','texture');  % show your matrix
 
-set(h,'alphadata',~isnan(flip(matrix_plot)));
-hc0=colorbar('southoutside');
-set(hc0,'position',[0.25 0.27 0.36 0.035]);
+set(h,'alphadata',~isnan(flip(matrix_plot))); % NAN pixel has no color.
+hc0 = colorbar('southoutside');  % the location of your color bar. Also, you can remove this line so that the colorbar won't show.
+set(hc0,'position',[0.25 0.27 0.36 0.035]);  % the location of your color bar. Also, you can remove this line so that the colorbar won't show.
 
-colormap(ax,map);
-%colormap(parula) %parula
+
+colormap(parula) % colormap
+% colormap(ax,map); % you can also set your exclusive colorbar, such as [143 180 104;255 201 104;200 138 112]/255
 
 land = shaperead('landareas', 'UseGeoCoords', true);
 geoshow(ax, land, 'FaceColor', 'none')
